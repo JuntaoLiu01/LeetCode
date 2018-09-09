01bdcba1270ee81d91ab9b9ca06a21772605e869
Eric <0001000000100100km@gmail.com>
2018-09-09 09:30:31 +0800

    lower_bound and upper_bound by myself

--- Array/FFLPESA.cpp
+++ Array/FFLPESA.cpp
@@ -18,18 +18,44 @@ public:
 class Solution2 {
 private:
     int lower(vector<int>& nums,int target){
-
+        int left = 0,right = nums.size();
+        int pos = 0;
+        while(left < right){
+            int mid = (left+right)/2;
+            if(nums[mid] < target){
+                left = mid + 1;
+                pos = left;
+            }
+            else{
+                right = mid;
+                pos = right;
+            }
+        }
+        return pos;
     }
     int upper(vector<int>& nums,int target){
-
+        int left = 0, right = nums.size();
+        int pos = 0;
+        while(left < right){
+            int mid = (left+right)/2;
+            if(nums[mid] > target){
+                right = mid;
+                pos = right;
+            }
+            else{
+                left = mid + 1;
+                pos = left;
+            }
+        }
+        return pos;
     }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int begin = lower(nums,target);
         int end = upper(nums,target);
         if(begin == end)
-            return {-1,-1};
+            return {-1,-1}; 
         else
-            return {begin,end};
+            return {begin,end-1};
     }
 };
