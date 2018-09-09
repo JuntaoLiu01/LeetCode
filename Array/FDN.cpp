d5a538eb1e72726af82f6d9759dec6f850b4d979
Eric <0001000000100100km@gmail.com>
2018-09-09 13:45:29 +0800

    slow and fast pointer for the problem

--- Array/FDN.cpp
+++ Array/FDN.cpp
@@ -21,3 +21,25 @@ public:
         return left;
     }
 };
+
+// O(n) method
+class Solution2 {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0,fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast)
+                break;
+        }
+        int start = 0;
+        while(true){
+            slow = nums[slow];
+            start = nums[start];
+            if(slow == start)
+                break;
+        }
+        return slow;
+    }
+};
