f14a9d0910ec72e88e202ccdd7701d63d8894438
Eric <0001000000100100km@gmail.com>
2018-09-10 19:56:10 +0800

    modify  K-diff Pairs in an Array

--- Array/KPA.cpp
+++ Array/KPA.cpp
@@ -11,7 +11,7 @@ public:
         for(int i = 0;i < nums.size();i++){
             if(i > 0 && nums[i] == nums[i-1])
                 continue;
-            if(binary_search(nums.begin()+i+1,nums.end(),nums[i]+k) != nums.end())
+            if(binary_search(nums.begin()+i+1,nums.end(),nums[i]+k))
                 res++;
         }
         return res;
